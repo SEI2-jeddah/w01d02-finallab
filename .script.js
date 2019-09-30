75cd2df77ec77f9f27cea24f390fec6b4cf45f3d
Shahad A. Bagarish <shahad.bagarish@gmail.com>
2019-09-30 17:01:51 +0300

    change

---
+++ .script.js
@@ -0,0 +1,56 @@
+let name = prompt("what is your name?")
+console.log(name)
+// let year = prompt("Nice to meet you, Ebere. What year would you like to go to?")
+// console.log(year)
+
+// if (year <= 1984 && year >= 1955){
+
+// 	let ask = prompt("I see you're a fan of Back to the Future 1. Your future Mom has just asked you to the Enchantment Under the Sea dance. What do you do? (Y/N/S)")
+// 	console.log(ask)
+
+// 	if (ask == 'Y'){
+
+// 		console.log("Creepy. I hope you have some backup plan in place to get out of this. Until then, you're stuck in 1955.")
+
+// 	}else if (ask == 'N'){
+
+// 		console.log("Honorable. But this also means that your future Dad will never meet your Mom, and therefore you cannot exist.")
+
+// 	}else if (ask == 'S'){
+
+// 		console.log("Interesting. You set up an elaborate plan for your future Dad to surprise your Mom by"+
+// 		 "beating you up. Despite going horribly awry, the plan ultimately works. You may go back to your own time.")
+// 	}
+// 	else{
+// 		console.log('Enter (Y/N/S)')
+// 	}
+  
+// }else if (year < 1955){
+// 	let ask = prompt("I see you're a fan of Back to the Future 3. You've run out of gas and can't get back to your own time! How do you power the Time Machine? (H/M/T)")
+// 	console.log(ask)
+
+// 	if (ask == 'H'){
+
+// 		console.log("Good idea, but no. The time machine needs to get to 88mph. 12 horsepower ain't gonna cut it.")
+
+// 	}else if (ask == 'M'){
+
+// 		console.log("You'd be better off drinking the moonshine. Do not pass Go, do not collect $200. Stuck in 1855.")
+
+// 	}else if (ask == 'T'){
+
+// 		let ask = prompt("Good call! This plan seems to be working. But wait! Clara wants to go Back to the Future with you at the last moment. What do you do? (T/L)")
+// 		console.log(ask)
+// 		if (ask == 'T'){
+
+// 			console.log("Interesting choice. Unfortunately the Doc can't grab Clara and get back to the car in time. He ends up staying in 1855 with her.")
+// 		}else if (ask == 'L') {
+// 			console.log("Smart choice. Unfortunately the Doc was deeply in love with Clara, and when he gets back to 1985 he becomes very depressed.")
+// 		}else {
+//         console.log('Enter (T/L)')
+//         }
+// 	}else{
+// 		console.log('Enter (H/M/T)')
+// 	}
+// }
+
