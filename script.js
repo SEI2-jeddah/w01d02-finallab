56134e6b6a5d4c5b3ab22c984751b9fbc0c668c2
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2019-09-30 16:42:38 +0300

    made game logic

--- script.js
+++ script.js
@@ -1,2 +1,63 @@
-let input = prompt("what is your name?")
-console.log(input)
+
+
+var input = prompt("what type of drink would you like to order \n 1.juce 2.coffee 3.tea ")
+
+if(input == "2"){
+
+input = prompt("what type of coffee would you like to order \n 1. latte 2.cappuccnio 3.esspreso ")
+  if(input == "1"){
+
+console.log("One latte right away!")
+  }else  if(input == "2"){
+
+console.log("One cappuccnio right away!")
+  }else  if(input == "3"){
+
+console.log("One esspreso right away!")
+  }
+
+
+//-------------------------
+
+
+}else if(input == "1"){
+
+input = prompt("what type of juce would you like to order \n 1.orange  2.apple 3.mango 4.strawberry ")
+if(input == "1"){
+
+console.log("One orange juice right away!")
+  }else  if(input == "2"){
+
+console.log("One apple juice right away!")
+  }else  if(input == "3"){
+
+console.log("One mango juice right away!")
+  }else  if(input == "4"){ 
+
+console.log("One strawberry juice right away!")
+  }
+
+
+//----------------
+
+
+
+}else if(input == "3"){
+
+input = prompt("what type of juce would you like to order \n 1.red tea  2.green tea 3.chinese tea 4.black tea ")
+
+
+if(input == "1"){
+
+console.log("One red tea right away!")
+  }else  if(input == "2"){
+
+console.log("One green tea right away!")
+  }else  if(input == "3"){
+
+console.log("One chinese tea right away!")
+  }else  if(input == "4"){ 
+
+console.log("One black tea right away!")
+  }
+}
